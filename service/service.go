d79f592c72dc97abce4b5a9ec071d6aefd81f32d
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-10-16 04:18:37 +0900

    add TrimSpace

--- service/service.go
+++ service/service.go
@@ -14,7 +14,10 @@ func Perform(com command.Command, options *utils.Options) {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
-		line := strings.Split(scanner.Text(), options.Delimiter)
+		line := []string{}
+		for _, v := range strings.Split(scanner.Text(), options.Delimiter) {
+			line = append(line, strings.TrimSpace(v))
+		}
 
 		if err := com.Perform(line); err != nil {
 			log.Println(err)
